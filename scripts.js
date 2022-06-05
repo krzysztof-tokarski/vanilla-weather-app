0dacd35760f4df4d3634b10693657c7a56519553
AEE <myfutureiscode@gmail.com>
2022-06-05 20:42:16 +0200

    feat: add basic fetching and error handling

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const main = document.querySelector('main');
 const submitButton = document.getElementById('submit-button');
 const weatherForm = document.getElementById('weather-form');
 const cityInput = document.getElementById('city');
@@ -53,26 +54,90 @@ function mapResponseData(response) {
 };
 
 function prepareShowcase() {
+  const showcase = document.createElement('div');
+  showcase.setAttribute('id', 'showcase');
+  
+  const cityInfo = document.createElement('div');
+  const cityInfoHeader = document.createElement('h2');
+  const cityName = document.createElement('p');
+  const countryCode = document.createElement('p');
+  cityInfoHeader.textContent = "CITY INFO";
+  cityName.textContent = currentCity.cityName;
+  countryCode.textContent = currentCity.countryCode;
+  cityInfo.appendChild(cityInfoHeader);
+  cityInfo.appendChild(cityName);
+  cityInfo.appendChild(countryCode);
+
+  const temperatureInfo = document.createElement('div');
+  const temperatureInfoHeader = document.createElement('h2');
+  const feelsLike = document.createElement('p');
+  const actual = document.createElement('p');
+  const min = document.createElement('p');
+  const max = document.createElement('p');
+  const pressure = document.createElement('p');
+  const humidity = document.createElement('p');
+  temperatureInfoHeader.textContent = "TEMPERATURE INFO";
+  feelsLike.textContent = `Feels like: ${currentCity.temperature.feelsLike}`;
+  actual.textContent = `Actual: ${currentCity.temperature.actual}`;
+  min.textContent = `Min: ${currentCity.temperature.min}`;
+  max.textContent = `Max: ${currentCity.temperature.max}`;
+  pressure.textContent = `Pressure: ${currentCity.temperature.pressure}`;
+  humidity.textContent = `Humidity: ${currentCity.temperature.humidity}`;
+  const temparatureArray = [temperatureInfoHeader, feelsLike, actual, min, max, pressure, humidity];
+  temparatureArray.forEach(data => {
+    temperatureInfo.appendChild(data);
+  })
+
+  const weatherInfo = document.createElement('div');
+  const weatherInfoHeader = document.createElement('h2');
+  const description = document.createElement('p');
+  weatherInfoHeader.textContent = 'WEATHER INFO';
+  description.textContent = `Weather description: ${currentCity.weather.description}`;
+  // const icon = 
   // TODO
+  weatherInfo.appendChild(weatherInfoHeader);
+  weatherInfo.appendChild(description);
+
+
+  const windInfo = document.createElement('div');
+  const windInfoHeader = document.createElement('h2');
+  const windSpeed = document.createElement('p');
+  const windDegree = document.createElement('p');
+  windInfoHeader.textContent = "WIND INFO";
+  windSpeed.textContent = `Wind speed: ${currentCity.wind.speed}`;
+  windDegree.textContent = `Wind speed: ${currentCity.wind.degree}`;
+  windInfo.appendChild(windInfoHeader);
+  windInfo.appendChild(windSpeed);
+  windInfo.appendChild(windDegree);
+
+  const sections = [cityInfo, temperatureInfo, weatherInfo, windInfo];
+  sections.forEach(index => {
+    const section = document.createElement('section');
+    section.appendChild(index);
+    showcase.appendChild(section);
+  });
+  
+  main.appendChild(showcase);
 }
 
 async function fetchCityWeatherData(cityInputValue) {
   const pull = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInputValue}&appid=${API_KEY}`);
   const response = await pull.json();
-  if  (response.length > 0) {
-    mapResponseData(response);
+  console.log(response);
 
+  if  (response.cod === 200) {
+    mapResponseData(response);
+    prepareShowcase();
   } else {
-    return alert("xD");
+    return console.log(response.cod, response.message);
     // TODO
   }
 };
 
 weatherForm.addEventListener('input', () => {
-
   const formValidityState = weatherForm.checkValidity();
 
-  if (!formValidityState) {
+  if (!formValidityState || cityInput.value.length < 3) {
     submitButton.disabled = true;
   } else {
     submitButton.disabled = false;
